cd11e63bf9c99cea00d57f86502621d2b6cbe71a
perceptiverelief <body64_boycott@icloud.com>
2026-02-12 12:13:56 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('relief-lab').then(cache => {
-      return cache.addAll(['./']);
-    })
-  );
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open('relief-ultra').then(cache=>cache.addAll(['./'])));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener('fetch',e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
