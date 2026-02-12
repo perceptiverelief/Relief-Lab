93d23880b081710198212dc3fe0c8413262a26a0
perceptiverelief <body64_boycott@icloud.com>
2026-02-12 13:14:49 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install',e=>{
-e.waitUntil(caches.open('relief-ultra').then(cache=>cache.addAll(['./'])));
-});
-self.addEventListener('fetch',e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
